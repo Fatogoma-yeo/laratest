7dd6333dfa87609e354ecf5604d297dfecc6a287
Yeo Fatogoma <yeofougnigue8@gmail.com>
2023-07-13 13:21:28 +0000

    order void function update

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -25,8 +25,8 @@ class DashboardController extends Controller
     {
         $userDetails = User::get();
 
-        $current_days = Orders::whereDay('created_at', Carbon::now())
-        ->orWhere(['payment_status' => 'paid', 'payment_status' => 'partially_paid' ])
+        $current_days = Orders::where(['payment_status' => 'paid', 'payment_status' => 'partially_paid' ])
+        ->whereDay('created_at', Carbon::now())
         ->select(
             DB::raw('SUM(tendered) as total_sales'),
             DB::raw('DATE_FORMAT(created_at,"%W") as day')
