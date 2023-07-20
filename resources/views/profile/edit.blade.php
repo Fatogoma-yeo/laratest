5ea279d642b81c187ba63573af72dc4c9ace22bb
Yeo Fatogoma <yeofougnigue8@gmail.com>
2023-07-20 13:53:12 +0000

    profile blade update

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,18 +1,23 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="flex justify-center text-lg">
+    <div class="py-4">
+        <div class="mx-auto">
+            <div class="px-4">
+                <div class="flex justify-center text-white text-xl">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
                 </div>
-                <div class="flex justify-between">
-                    <div class="max-w-xl">
-                        @include('profile.partials.update-profile-information-form')
+                <div class="md:flex justify-col gap-4">
+                    <div class="p-4 sm:p-8 bg-white shadow rounded-lg my-4 md:w-1/2">
+                        <div class="">
+                            @include('profile.partials.update-profile-information-form')
+                        </div>
                     </div>
-                    <div class="max-w-xl">
-                        @include('profile.partials.update-password-form')
+
+                    <div class="p-4 sm:p-8 bg-white shadow rounded-lg my-4 md:w-1/2">
+                        <div class="">
+                            @include('profile.partials.update-password-form')
+                        </div>
                     </div>
                 </div>
             </div>
